d24b7ba6369ac7553ad1f289135a2361bd82a054
Arnaud <arnaud@status.im>
2024-08-20 15:57:59 +0200

    Add space allocation component

---
+++ stories/SpaceAllocation.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SpaceAllocation } from "../components/SpaceAllocation/SpaceAllocation";
+
+const meta = {
+  title: "Advanced/SpaceAllocation",
+  component: SpaceAllocation,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof SpaceAllocation>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    data: [
+      {
+        title: "Maximum storage space used by the node",
+        percent: 60,
+        size: 10000000,
+      },
+      {
+        title: "Amount of storage space currently in use",
+        percent: 20,
+        size: 10000000 * 0.2,
+      },
+      {
+        title: "Amount of storage space reserved",
+        percent: 20,
+        size: 10000000 * 0.2,
+      },
+    ],
+  },
+};
