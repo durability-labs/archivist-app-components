68907ce2f3255dc5b056b709041b8d350268cecf
Arnaud <arnaud@status.im>
2024-08-20 16:10:02 +0200

    Fix worker build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   worker: {
-    format: "es",
+    rollupOptions: {
+      external: ["@codex/sdk-js"],
+    },
   },
 });
